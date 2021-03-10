ae5df2663840f8906d5d29e00f94bbf82fe67d93
oneema <79211762+oneema@users.noreply.github.com>
2021-03-10 11:08:33 +0530

    Update python_calculator.py

--- python_calculator.py
+++ python_calculator.py
@@ -28,11 +28,11 @@ def divide(x, y):
    return x / y
 
 # take input from the user
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+# print("Select operation.")
+# print("1.Add")
+# print("2.Subtract")
+# print("3.Multiply")
+# print("4.Divide")
 
 # choice = input("Enter choice(1/2/3/4):")
 
